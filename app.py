05c82893de178122e2ec5c6bc21df3f9a3dd4605
ricky-sensei <axcelcamp.teacher@gmail.com>
2025-08-30 01:02:51 +0900

    mod message

--- app.py
+++ app.py
@@ -40,7 +40,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    user_message = event.message.text
+    reply_message = user_message[7:-5]
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
 
 if __name__ == "__main__":
